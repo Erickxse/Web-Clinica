183cee2ef7ad78a27c52f4da4987279cbcf25a3e
erickxse <erick_sebastian_@hotmail.com>
2024-01-25 22:23:25 -0500

    size body

--- js/start.js
+++ js/start.js
@@ -1,10 +1,14 @@
 //start.js
 
-  document.addEventListener('DOMContentLoaded', function () {
-    var navbar = document.getElementById('navbar');
-    var burgerIcon = document.querySelector('.burger-icon');
 
-    burgerIcon.addEventListener('click', function () {
-      navbar.classList.toggle('hide-navbar');
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  var navbar = document.getElementById('navbar');
+  var contentSection = document.querySelector('.content');
+  var burgerIcon = document.querySelector('.burger-icon');
+
+  burgerIcon.addEventListener('click', function () {
+    navbar.classList.toggle('hide-navbar');
+    contentSection.classList.toggle('expand-content');
   });
+});
+
