6f7fae1691f52fcaa16986f538cb26e0da546629
erickxse <erick_sebastian_@hotmail.com>
2024-02-08 05:14:14 -0500

    calendar

--- js/start.js
+++ js/start.js
@@ -6,40 +6,37 @@ document.addEventListener('DOMContentLoaded', function () {
   var imgTitulo = document.querySelector('.img-titulo');
   var imgLogo = document.querySelector('.img-logo');
   var dropdownMenu = document.getElementById('dropdownMenu');
-  var body = document.body;
-  
-
   const navOptions = document.querySelectorAll('#navOptions li');
-  
+
+
   burgerIcon.addEventListener('click', function () {
     navbar.classList.toggle('shrink-navbar');
     contentSection.classList.toggle('expand-content');
+    document.body.classList.toggle('shrink-body');
 
-    body.classList.toggle('shrink-body');
-    
     imgTitulo.classList.add('shrink');
 
     navOptions.forEach(option => {
       option.classList.toggle('hide-text');
     });
 
-    var menuText = document.getElementById('menuText'); 
+    var menuText = document.getElementById('menuText');
     menuText.classList.toggle('hide-text');
     setTimeout(function () {
       if (imgTitulo.src.includes('ClinicaRenacer.png')) {
-        imgTitulo.src = '../images/estetoscopio-logo.png'; 
+        imgTitulo.src = '../images/estetoscopio-logo.png';
         imgTitulo.alt = 'Estetoscopio';
-        imgTitulo.style.height = '30px'; 
+        imgTitulo.style.height = '30px';
         imgTitulo.style.width = '30px';
       } else {
-        imgTitulo.src = '../images/ClinicaRenacer.png'; 
+        imgTitulo.src = '../images/ClinicaRenacer.png';
         imgTitulo.alt = 'Titulo';
-        imgTitulo.style.height = '40px'; 
+        imgTitulo.style.height = '40px';
         imgTitulo.style.width = '300px';
       }
 
       imgTitulo.classList.remove('shrink');
-    }, 300); 
+    }, 300);
   });
 
   var isDropdownVisible = false;
@@ -53,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     isDropdownVisible = !isDropdownVisible;
   });
-  
+
   imgLogo.addEventListener('mouseover', function () {
     imgLogo.classList.add('flip');
     imgLogo.src = '../images/engranaje-logo.png';
@@ -64,16 +61,27 @@ document.addEventListener('DOMContentLoaded', function () {
     imgLogo.src = '../images/clinic-logo.png';
   });
 
-
   navOptions.forEach(option => {
+    option.addEventListener('mouseover', function (event) {
+      var title = this.getAttribute('title');
+      if (title) {
+        var tooltip = document.createElement('span');
+        tooltip.classList.add('tooltip');
+        tooltip.textContent = title;
+        document.body.appendChild(tooltip);
+        tooltip.style.top = `${event.clientY + 10}px`;
+        tooltip.style.left = `${event.clientX + 10}px`;
+
+        option.addEventListener('mouseout', function () {
+          document.body.removeChild(tooltip);
+        });
+      }
+    });
+
     option.addEventListener('click', function () {
-      var pagePath = this.dataset.page; // Asegúrate de tener el atributo data-page en tus elementos li
-      
-      // Restaltar la opción seleccionada
+      var pagePath = this.dataset.page;
       navOptions.forEach(opt => opt.classList.remove('active'));
       this.classList.add('active');
-
-      // Redirigir a la página correspondiente
       window.location.href = pagePath;
     });
   });
